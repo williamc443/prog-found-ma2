7df95a0ae2375d783df289c04fab68c339cca981
williamc443 <57712080+williamc443@users.noreply.github.com>
2020-01-12 23:39:25 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+// question 1
+
+var petArrey = ["cat", "cow", "dog"];
+
+// question 2
+
+console.log(petArrey[1]);
+
+//question 3
+
+petArrey.push("sheep");
+
+//question 4
+
+console.log(petArrey.length);
+
+// question 5
+
+var catObject = {
+  name: "rosie",
+  colour: "black",
+  age: 5
+};
+
+// question 6
+
+console.log(catObject[2]);
+
+// question 7
+
+var catArrey = [
+  {
+    name: "rosie",
+    colour: "black",
+    age: 5
+  }
+];
+
+// question 8
+
+for (var i = 0; i < catArrey.length; i++) {
+  console.log(catArrey[i]);
+}
+
+// question 9
+
+function logToConsole(trueorfalse) {
+  console.log(true);
+}
+
+// question 10
+
+logToConsole("donkey");
